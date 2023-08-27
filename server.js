1b20e5537316ce325cbc46e46645c9b59e1dc65b
fenil nakrani <fenilnakrani123@gmail.com>
2023-08-27 14:58:46 -0400

    updates

--- server.js
+++ server.js
@@ -3,35 +3,17 @@ require('dotenv').config
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const path = require('path')
-const {logger} =require('./middleware/logger')
 const corsOptions =require('./config/corsOptions')
 const dbConnection = require('./config/dbConnection')
 const mongoose = require('mongoose'); 
 const serverless = require('serverless-http');
 const PORT=process.env.PORT || 9000;
 var pjson = require('./package.json');
-const swaggerUi = require('swagger-ui-express');
-const swaggerjsDoc = require('swagger-jsdoc');
 const app =express()
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Content Management System',
-            description: 'Content Management System API Information',
-            contact: {
-                name: "Amazing Developer"
-            },
-            servers: ["http://localhost:3000"]
-        }
-    },
-    apis: ["./routes/*.js"]
-}
 
-const swaggerDocs = swaggerjsDoc(swaggerOptions);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-// app.use(logger)
+
 app.use(cors(corsOptions))
 app.use(cookieParser());
 app.use(express.json());
