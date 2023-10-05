1f329bc9ef39d162ca6a075197d5ba975403aff0
Fenil Nakrani <fenilnakrani123@gmail.com>
2023-10-05 17:36:19 +0530

    Added forgot password functionality and clean-up code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,5 @@
 const User = require('../models/model.user');
 const Content = require('../models/model.content');
-const jwt = require('jsonwebtoken')
-
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const { json } = require('body-parser');
@@ -16,48 +14,7 @@ const getAllUsers = asyncHandler(async (req, res) => {
         return res.status(404).json({ message: "No users found" });
     }
     res.json(users);
-})
-
-
-// @desc    create users
-// @route   POST /users
-// @access  Private
-
-const createNewUser = asyncHandler(async (req, res) => {
-    const { email, username, name, password } = req.body;
-    const roles = ["admin"];
-    // check data
-    console.log(email, username, name, password);
-    if (!email || !username || !name || !password) {
-        return res.status(400).json({ message: "Please enter all fields" });
-    }
-
-    // check if user already exists
-    const duplicate_email = await User.findOne({ email }).lean().exec();
-    if (duplicate_email) {
-        return res.status(409).json({ message: "Email is taken" });
-    }
-
-    const duplicate_username = await User.findOne({ email }).lean().exec();
-    if (duplicate_username) {
-        return res.status(409).json({ message: "Username is already taken" });
-    }
-
-    // hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const userObject = { email, username, name, "password": hashedPassword, roles };
-
-    // create user
-    const user = await User.create(userObject);
-    if (!user) {
-        res.status(500).json({ message: "Something went wrong" });
-    } else {
-        // console.log("Admin is created");
-        res.status(201).json({ message: `${user} created successfully` });
-        // res.status(201).json({message: "User created successfully"});
-    }
-})
+});
 
 // @desc    update user
 // @route   POST /user/:userId/update
@@ -65,9 +22,8 @@ const createNewUser = asyncHandler(async (req, res) => {
 const updateUser = asyncHandler(async (req, res) => {
     const { email, name, username } = req.body;
 
-
     if (!email || !name, !username) {
-        return res.status(400).json({ message: "Email and Name and Username is required" });
+        return res.status(400).json({ message: "Email, Name and Username is required" });
     }
 
     const user = await User.findOne({ _id: req.params.userId }).exec();
@@ -96,14 +52,13 @@ const updateUser = asyncHandler(async (req, res) => {
         res.status(200).json({ message: `${name} updated successfully}` });
     }
 
-})
+});
 
 // @desc    update Password
 // @route   POST /user/:userId/updatePassword
 // @access  Private
 const updatePassword = asyncHandler(async (req, res) => {
     const { password } = req.body;
-
 
     if (!password) {
         return res.status(400).json({ message: "Password is required" });
@@ -125,24 +80,18 @@ const updatePassword = asyncHandler(async (req, res) => {
     } else {
         res.status(200).json({ message: `Password updated successfully}` });
     }
-
-})
+});
 
 // @desc    delete user
 // @route   POST /users
 // @access  Private
 const deleteUser = asyncHandler(async (req, res) => {
-    const { id } = req.body;
-    console.log("Id is " + id);
-    if (!id) {
+    const userId = req.params.userId;
+    if (!userId) {
         return res.status(400).json({ message: "User ID is required" });
     }
-    //  check if user has content in the database
-    // if yes then ask for delete content and then delete user
-    // logic add remaining
-    const content = await Content.find({ User: id }).lean().exec();
 
-    const user = await User.findById(id).exec();
+    const user = await User.findById(userId).exec();
     if (!user) {
         return res.status(404).json({ message: "User not found" });
     }
@@ -153,84 +102,18 @@ const deleteUser = asyncHandler(async (req, res) => {
     } else {
         res.status(200).json({ message: `${user.name} deleted successfully}` });
     }
-})
+});
 
 const getUser = asyncHandler(async (req, res) => {
-    // const user = req.body;
     const userId = req.params.userId;
-    // const email = req.query.email; // domaim.com/uri?email=xyz&name=abc
-    console.log(userId);
-    console.log("get user profile by ID called");
-    // const token = req.cookies.jwt;
-    // jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, async (err, decodedToken) => {
-    //     if (err) {
-    //         console.log(err.message);
-    //         res.locals.user = null;
-    //         res.status(401).json({message: "Unauthorized"});
-    //     } else {
-    //         console.log(decodedToken);
-    //     }});
-    const user= await User.findOne({userId}).lean().exec();
-    console.log(user);
+    const user = await User.findById(userId).lean().exec();
     return res.status(200).json({ message: user });
-    // const {email}=req.body;
-    //     console.log("Id is "+email);
-    //     if(!email){
-    //         return res.status(400).json({message: "User Email is required"});
-    //     }
-
-    //     if(!user){
-    //         return res.status(404).json({message: "User not found"});
-    //     }
-
-    //     res.status(200).json({user});
-})
-
-
-// @desc    delete user
-// @route   POST /users
-// @access  Public // no JWT required for signup as this is used for normal user 
-//sighup and not for admin
-const signupUser = asyncHandler(async (req, res) => {
-    const { email, name, password } = req.body;
-    const roles = ["user"];
-    // check data
-    console.log(email, name, password);
-    if (!email || !name || !password) {
-        return res.status(400).json({ message: "Please enter all fields" });
-    }
-    // return res.status(201).json({message: `${name} created successfully`});
-
-    // check if user already exists
-    const duplicate = await User.findOne({ email }).lean().exec();
-    if (duplicate) {
-        return res.status(409).json({ message: "User already exists" });
-    }
-
-    // hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const userObject = { email, name, "password": hashedPassword, roles };
-
-    // create user
-    const user = await User.create(userObject);
-    if (!user) {
-        console.log("user not created")
-        res.status(500).json({ message: "Something went wrong" });
-    } else {
-        console.log("Normal User is created");
-        res.status(201).json({ message: `${user} created successfully` });
-        // res.status(201).json({message: "User created successfully"});
-    }
-})
-
+});
 
 module.exports = {
     getAllUsers,
-    createNewUser,
     updateUser,
     deleteUser,
     getUser,
-    signupUser,
     updatePassword
 }
--- server.js
+++ server.js
@@ -1,51 +1,42 @@
 const express = require('express')
-require('dotenv').config
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const path = require('path')
-const corsOptions =require('./config/corsOptions')
+const corsOptions = require('./config/corsOptions')
 const dbConnection = require('./config/dbConnection')
-const mongoose = require('mongoose'); 
+const userRoutes = require('./routes/userRoutes')
+const authRoutes = require('./routes/authRoutes')
+const subdomainRoute = require('./routes/subdomainRoute')
+const initRoutes = require('./routes/initRoutes')
+const contentRoutes = require('./routes/contentRoutes')
+const mongoose = require('mongoose');
 const serverless = require('serverless-http');
-const PORT=process.env.PORT || 9000;
-var pjson = require('./package.json');
-const app =express()
-
-
-
+const PORT = process.env.PORT || 9000;
+const app = express()
+require('dotenv').config
 
 app.use(cors(corsOptions))
 app.use(cookieParser());
 app.use(express.json());
 
+dbConnection();
 
-dbConnection()
-app.get('/',(req,res)=>{
-   console.log(req.hostname)
-    res.redirect('/url/test/url');
-    // ("hello world version : "+pjson.version)
-})
-app.get('/url/test/url',(req,res)=>{
-    console.log(req.hostname)
-     res.send(`<h1>hello world version : <h1>`+pjson.version)
- })
-app.use('/user',require('./routes/userRoutes'))
-app.use('/auth',require('./routes/authRoutes'))
-app.use('/subdomain',require('./routes/subdomainRoute'))
-app.use('/init',require('./routes/initRoutes'))
-app.use('/content',require('./routes/contentRoutes'))
+app.use('/user', userRoutes);
+app.use('/auth', authRoutes);
+app.use('/subdomain', subdomainRoute);
+app.use('/init', initRoutes);
+app.use('/content', contentRoutes);
 
-mongoose.connection.once('open',()=>{
+mongoose.connection.once('open', () => {
     console.log("MongoDB connection established successfully");
-    app.listen(PORT,()=>{
-        console.log("server running on port "+PORT);
-        console.log("http://localhost:"+PORT);
-    })
-})
+    app.listen(PORT, () => {
+        console.log("server running on port " + PORT);
+        console.log("http://localhost:" + PORT);
+    });
+});
 
-mongoose.connection.on('error',(err)=>{
-    console.log("MongoDB connection error. Please make sure MongoDB is running. "+err);
+mongoose.connection.on('error', (err) => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
     process.exit();
-})
+});
 
 module.exports.handler = serverless(app);
